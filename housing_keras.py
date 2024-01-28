fc7802c009ce57178ab7176a60b43b5237fd6fe3
BHariKrishnaReddy <reddyfrn007@gmail.com>
2024-01-28 15:35:34 -0500

    Tf coding

--- housing_keras.py
+++ housing_keras.py
@@ -14,14 +14,14 @@ X_valid = scaler.transform(X_valid)
 X_test = scaler.transform(X_test)
 
 model = keras.models.Sequential([
-    keras.layers.Dense(30, activation="relu", input_shape=X_train.shape[1:]),
-    keras.layers.Dense(1)
-                              ])
+	keras.layers.Dense(30, activation="relu", input_shape=X_train.shape[1:]),
+	keras.layers.Dense(1)
+])
 
 model.compile(loss="mean_squared_error", optimizer="sgd")
 
-history = model.fit(X_train, y_train, epochs=20,validation_data=(X_valid, y_valid))
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
 
 mse_test = model.evaluate(X_test, y_test)
-X_new = X_test[:3] # pretend these are new instances
+X_new = X_test[:3]  # pretend these are new instances
 y_pred = model.predict(X_new)
